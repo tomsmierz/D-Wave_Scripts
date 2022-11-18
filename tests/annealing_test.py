05afc68cc18e03bc2f9cc9fdf622cb835b1ce683
tomsmierz <smirzchalski.tomek@gmail.com>
2022-11-18 11:36:02 +0100

    initial commit

---
+++ tests/annealing_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+"""
+Not written yet
+"""
+
+class TestEnergy(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
