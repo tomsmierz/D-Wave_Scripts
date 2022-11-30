66fcf7bee3305326a42b3ee74a0a862aac623859
tomsmierz <smirzchalski.tomek@gmail.com>
2022-11-30 12:36:51 +0100

    user frendly changes

--- src/anneal.py
+++ src/anneal.py
@@ -10,14 +10,17 @@ problem_run_duration_range = [0.0, 1000000.0] microseconds
 
 sampler = DWaveSampler(solver="Advantage_system6.1")
 
+instance_number = "003"
+size = 4
 for category in ["AC3", "RCO", "RAU"]:
-    with open(f"../instances/pegasus_random/P4/{category}/001_dv.pkl", "rb") as f:
+    with open(f"../instances/pegasus_random/P{size}/{category}/{instance_number}_dv.pkl", "rb") as f:
         h, J = pickle.load(f)
 
     for annealing_time in [2, 200, 2000]:
         num_reads = 300 if annealing_time == 2000 else 1000
-        sample_set = sampler.sample_ising(h, J, annealing_time=annealing_time, num_reads=num_reads, auto_scale=False)
+        sample_set = sampler.sample_ising(h, J, annealing_time=annealing_time, num_reads=num_reads, auto_scale=False,
+                                          label=f"pegasus P{size} {instance_number} {category} {annealing_time}")
         df = sample_set.to_pandas_dataframe()
-        df.to_csv(f"../energies/pegasus_random/P4/{category}/001_{annealing_time}_{num_reads}.csv")
+        df.to_csv(f"../energies/pegasus_random/P{size}/{category}/{instance_number}_{annealing_time}_{num_reads}.csv")
 
 
