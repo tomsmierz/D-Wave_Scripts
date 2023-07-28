8c1ffc180641103770eebce1ca586a5bee8f9f70
tomsmierz <smierzchalski.tomek@gmail.com>
2023-07-28 17:45:18 +0200

    new loop

--- src/anneal_new.py
+++ src/anneal_new.py
@@ -11,19 +11,21 @@ if __name__ == '__main__':
     token = "jszS-a65db66e0d37bf62c83f45adad63cd25c5f8dc53"
     for topology in ["pegasus", "zephyr"]:
 
-        sizes = [4, 8, 12, 16] if topology == "pegasus" else [2, 3, 4] #[4, 8, 12]
+        sizes = [4, 8, 12, "16_6.2"] if topology == "pegasus" else [2, 3, 4] #[4, 8, 12]
         sampler = (
             DWaveSampler(solver="Advantage_system6.2", token=token)
             if topology == "pegasus"
             else DWaveSampler(solver="Advantage2_prototype1.1", token=token)
         )
-        sampler = LazyFixedEmbeddingComposite(sampler)
         symbol = "P" if topology == "pegasus" else "Z"
         QCP = 0.3 if topology == "pegasus" else 0.25
         for size in sizes:
-            for i in tqdm(range(1, 101)):
+            for i in tqdm(range(1, 101), desc=f"{topology} {size} {i}"):
                 instance_number = f"00{i}"[-3:]
-                for category in ["AC3", "RCO", "RAU", "CBFM-P"]:
+                categories = ["AC3", "RCO", "RAU"]
+                if topology == "pegasus":
+                    categories += ["CBFM-P"]
+                for category in categories:
                     with open(
                         f"../instances/{topology}_random/{symbol}{size}/{category}/{instance_number}_dv.pkl",
                         "rb",
@@ -32,7 +34,10 @@ if __name__ == '__main__':
 
                     annealing_time = 2000
                     # pause = 200
-                    for pause in [100, 300, 400,600, 700, 800, 900, 1100, 1200, 1300, 1400, 1600, 1700, 1800, 1900]:
+                    pauses = [100, 300, 400, 600, 700, 800, 900, 1100, 1200, 1300, 1400, 1600, 1700, 1800, 1900]
+                    if size == "16_6.2":
+                        pauses += [200, 500, 1000, 1500]
+                    for pause in pauses:
                         anneal_schedule = [[0, 0], [(annealing_time - pause)/2, QCP], [(annealing_time + pause)/2, QCP],
                                           [annealing_time, 1]]
                         reverse_anneal = [[0, 1], [(annealing_time - pause)/2, QCP], [(annealing_time + pause)/2, QCP],
@@ -46,7 +51,7 @@ if __name__ == '__main__':
                                 anneal_schedule=anneal_schedule if anneal == "forward" else reverse_anneal,
                                 num_reads=num_reads,
                                 auto_scale=False,
-                                label=f"{topology} {symbol}{size} {instance_number} {category} {annealing_time}",
+                                label=f"{topology} {size} {instance_number} {category} {annealing_time}",
                                 initial_state=initial_state if anneal == "reverse" else None,
                                 reinitialize_state=True if anneal == "reverse" else False
                             )
