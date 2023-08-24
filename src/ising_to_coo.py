0381a9c42daecb1679f9518ac7abb76e7cabf61e
tomsmierz <smirzchalski.tomek@gmail.com>
2023-08-24 16:33:35 +0200

    small changes

--- src/ising_to_coo.py
+++ src/ising_to_coo.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     for i in tqdm(range(1, 101)):
         name = f"00{i}"[-3:]
         name_full = name + "_sg.txt"
-        instance = pd.read_csv(os.path.join(cwd, "..", "instances", "pegasus_random", "P16", "CBFM-P", name_full),
+        instance = pd.read_csv(os.path.join(cwd, "..", "instances", "zephyr_random", "Z2", "RCO", name_full),
                                sep=" ", index_col=False, header=None, comment="#", names=["s1", "s2", "v"])
         h = {}
         J = {}
@@ -25,16 +25,10 @@ if __name__ == "__main__":
                 h[int(row.s1)-1] = int(row.v)
             else:
                 J[(int(row.s1)-1, int(row.s2)-1)] = int(row.v)
-        print(len(h.keys()))
-        for i in range(5400):
-            if i not in h.keys():
-                h[i] = 0
-                J[(i, i+1)] = 0
-        print(len(h.keys()))
         bqm = dimod.BinaryQuadraticModel(vartype="SPIN")
         bqm = bqm.from_ising(h, J)
-        print(bqm.num_variables)
-        with open(os.path.join(cwd, "..", f"instances/pegasus_random/P16/CBFM-P/COO/{name}.txt"), "w") as f:
+
+        with open(os.path.join(cwd, "..", f"instances/zephyr_random/Z2/RCO/COO/{name}.txt"), "w") as f:
             coo.dump(bqm, f)
 
 
